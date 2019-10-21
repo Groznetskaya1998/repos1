cda8edbaa9d01741007da2225dff5962452112a4
groznetskaya_k_ <grozneckaya.kseniya@bk.ru>
2019-10-21 21:49:14 +0500

    v2

--- 123.cpp
+++ 123.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
-
 using namespace std;
-
 int main(){
-      cout<<"Hello, World!"<<endl;
- 
+   char str[100];
+   cout<<"ВВедите"<<endl;
+   cin>>str;
+   cout<<"Hello, World"<<endl;
    return 0;
 }
